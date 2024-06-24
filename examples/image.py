e2c7759db79b350268be467c392733369fc9ed64
Chris Handy <maximinus@gmail.com>
2024-06-24 16:11:41 +0100

    Fixed image and label examples

--- examples/image.py
+++ examples/image.py
@@ -1,6 +1,6 @@
 import pygame
 from pyui.setup import init, await_keypress, DEFAULT_SIZE
-from pyui.base import Margin, get_asset
+from pyui.base import get_asset, Margin, Position
 from pyui.widgets import Image, Border
 
 BACKGROUND_COLOR = (80, 80, 80)
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     screens = [screen1()]
     for single_screen in screens:
         display.fill(BACKGROUND_COLOR)
-        single_screen.render(display, 100, 100, DEFAULT_SIZE)
+        single_screen.render(display, Position(100, 100), DEFAULT_SIZE)
         pygame.display.flip()
         await_keypress()
     pygame.quit()
--- examples/labels.py
+++ examples/labels.py
@@ -1,31 +1,31 @@
 import pygame
 from pyui.setup import init, await_keypress, DEFAULT_SIZE
-from pyui.base import Expand, Color, Align
+from pyui.base import Expand, Position, Align
 from pyui.widgets import TextLabel, VBox, HBox
+from pyui.theme import THEME
 
 BACKGROUND_COLOR = (80, 80, 80)
-
 # we will set up functions to display color widgets
 
 
 def screen1():
     # 1 widget aligned to the centre of the screen
-    return TextLabel('Hello, World!', font_size=32, expand=Expand.BOTH, align=Align.CENTER)
+    return TextLabel('Hello, World!', THEME.text['default'], expand=Expand.BOTH, align=Align.CENTER)
 
 
 def screen2():
     # a vbox with 3 centered labels of different sizes and colors
-    box = VBox(widgets=[TextLabel('First label', font_size=20, color=Color.RED, expand=Expand.BOTH, align=Align.CENTER),
-                        TextLabel('Second label', font_size=30, color=Color.GREEN, expand=Expand.BOTH, align=Align.CENTER),
-                        TextLabel('Third label', font_size=40, color=Color.BLUE, expand=Expand.BOTH, align=Align.CENTER)])
+    box = VBox(widgets=[TextLabel('First label', THEME.text['default'], expand=Expand.BOTH, align=Align.CENTER),
+                        TextLabel('Second label', THEME.text['default'], expand=Expand.BOTH, align=Align.CENTER),
+                        TextLabel('Third label', THEME.text['default'], expand=Expand.BOTH, align=Align.CENTER)])
     return box
 
 
 def screen3():
     # a hbox, 2 labels, aligned top to bottom
-    box = HBox(widgets=[TextLabel('First label', font_size=32, color=Color.RED, expand=Expand.BOTH, align=Align.CENTER|Align.TOP),
-                        TextLabel('Second label', font_size=32, color=Color.GREEN, expand=Expand.BOTH, align=Align.CENTER),
-                        TextLabel('Third label', font_size=32, color=Color.BLUE, expand=Expand.BOTH, align=Align.CENTER|Align.BOTTOM)])
+    box = HBox(widgets=[TextLabel('First label', THEME.text['default'], expand=Expand.BOTH, align=Align.CENTER|Align.TOP),
+                        TextLabel('Second label', THEME.text['default'], expand=Expand.BOTH, align=Align.CENTER),
+                        TextLabel('Third label', THEME.text['default'], expand=Expand.BOTH, align=Align.CENTER|Align.BOTTOM)])
     return box
 
 
@@ -34,7 +34,7 @@ if __name__ == '__main__':
     screens = [screen1(), screen2(), screen3()]
     for single_screen in screens:
         display.fill(BACKGROUND_COLOR)
-        single_screen.render(display, 0, 0, DEFAULT_SIZE)
+        single_screen.render(display, Position(0, 0), DEFAULT_SIZE)
         pygame.display.flip()
         await_keypress()
     pygame.quit()
