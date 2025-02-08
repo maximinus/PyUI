939d7f0ea74d5f69873294da5f553b7ea449bdc6
Chris Handy <maximinus@gmail.com>
2025-02-08 20:02:38 +0000

    Up-to-date

--- test/events/test_add_callbacks.py
+++ test/events/test_add_callbacks.py
@@ -5,19 +5,20 @@ from pyui.events.loop import get_ordered_callbacks, app
 from pyui.base import Position, Size, Color
 from pyui.widgets import Frame, Menu, ColorRect, HBox, MenuBar, MenuItem
 
+from pyui.events.queue import ev_queue
+
 
 class TestCallbacksAdded(unittest.TestCase):
     def test_menu_has_callback(self):
         test_menu = Menu(Position(0, 0), items=[])
-        self.assertEqual(len(test_menu.callbacks), 2)
+        # 2 events should be being listened out for
+        self.assertEqual(len(ev_queue.event_listeners), 2)
 
     def test_menu_callback_exists(self):
-        # add a menu to a frame; this has a callback
         test_menu = Menu(Position(0, 0), items=[])
-        test_frame = Frame(Position(0, 0), widget=test_menu)
-        # there is a callback by default in the menu, check we find it
-        callbacks = get_ordered_callbacks(test_frame)
-        self.assertEqual(len(callbacks), 2)
+        # we should find these events
+        self.assertTrue(Event.MouseIn, ev_queue.event_listeners)
+        self.assertTrue(Event.MouseOut, ev_queue.event_listeners)
 
     def test_has_added_callback(self):
         color = ColorRect(Size(50, 50), Color.RED)
@@ -28,16 +29,16 @@ class TestCallbacksAdded(unittest.TestCase):
         color = ColorRect(Size(50, 50), Color.RED)
         color.connect(Event.MouseMove, self.test_menu_has_callback)
         test_frame = Frame(Position(0, 0), widget=color)
-        callbacks = get_ordered_callbacks(test_frame)
-        self.assertEqual(len(callbacks), 1)
+        ev_queue.get_frame_callbacks(test_frame)
+        self.assertEqual(len(ev_queue.event_listeners), 1)
 
     def test_menu_bar(self):
         menubar = MenuBar()
         menubar.add_menu('File', Menu(items=[MenuItem('Help')]))
         window = Frame(size=Size(800, 600), widget=menubar)
-        app.push_frame(window)
+        ev_queue.get_frame_callbacks(window)
         callbacks = app.window_data[0].get_filtered_callbacks([Event.MouseLeftClickDown])
-        self.assertEqual(len(callbacks), 1)
+        self.assertEqual(len(ev_queue.event_listeners), 3)
 
 
 class TestCallbackOrder(unittest.TestCase):
